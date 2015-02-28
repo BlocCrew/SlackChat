a69be4ae99b4f22e51a6a1eb7d13aaff72129b4c
norway240 <conman240@gmail.com>
2015-02-28 00:35:57 -0600

    Initial Release. Basic 2 way communication working.

---
+++ com/norway240/slack/Comms.java
@@ -0,0 +1,169 @@
+package com.norway240.slack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+
+import org.bukkit.Bukkit;
+import org.json.simple.JSONObject;
+
+public class Comms {
+
+	private ServerSocket server;
+    private int PORT;
+	private String WEBHOOK;
+	private String VERIFICATION;
+	
+	public Comms(String w, String v, int p){
+		WEBHOOK = w;
+		VERIFICATION = v;
+		PORT = p;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void send(String name, String msg) throws IOException{
+
+		URL object=new URL(WEBHOOK);
+		HttpURLConnection con = (HttpURLConnection) object.openConnection();
+		con.setDoOutput(true);
+		con.setDoInput(true);
+		con.setRequestProperty("Content-Type", "application/json");
+		con.setRequestProperty("Accept", "application/json");
+		con.setRequestMethod("POST");
+		
+		JSONObject jobj = new JSONObject();
+		jobj.put("username", name);
+		jobj.put("icon_url", "http://signaturecraft.us/avatars/5/face/"+name+".png");
+		jobj.put("text", msg);
+
+		OutputStreamWriter wr= new OutputStreamWriter(con.getOutputStream());
+		wr.write(jobj.toString());
+		wr.flush();
+		
+		//display what returns the POST request
+		StringBuilder sb = new StringBuilder();  
+		int HttpResult =con.getResponseCode(); 
+		if(HttpResult ==HttpURLConnection.HTTP_OK){
+		    BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(),"utf-8"));  
+		    String line = null;  
+		    while ((line = br.readLine()) != null) {  
+		    sb.append(line + "\n");  
+		    }  
+		    br.close();  
+		}
+	}
+    
+	public void receive(){
+		new Thread(new Runnable() {
+	        public void run(){
+	        	try {
+	                server = new ServerSocket(PORT);
+	                System.out.println("[SlackChat] message receiving server active " + PORT);
+	                while (true) {
+	                    new ThreadSocket(server.accept());
+	                }
+	            }catch(Exception e){
+	            	System.out.println("[SlackChat] " + e.getStackTrace());
+	            }
+	       }
+	    }).start();
+	}
+	public void close(){
+		try {
+			server.close();
+		} catch (IOException e) {
+        	System.out.println("[SlackChat] " + e.getStackTrace());
+		}
+	}
+	
+	public void chat(String s){
+		//System.out.println(s);
+		String token = "";
+		String name = "", msg = "";
+		String vars[] = s.split("&");
+		for(String d : vars){
+			String data[] = d.split("=");
+			if(data[0].contains("user_name")){
+				name = data[1];
+			}else if(data[0].contains("text")){
+				msg = data[1];
+				try{msg = java.net.URLDecoder.decode(msg, "UTF-8");
+				}catch(UnsupportedEncodingException e){e.printStackTrace();}
+    			msg = msg.replace("&amp;", "&");
+			}else if(data[0].contains("token")){
+				token = data[1];
+			}
+		}
+		
+		if(token.contains(VERIFICATION)){
+	    	boolean slackbot = (name.toString().equals("slackbot"));
+		    if(!slackbot){
+		    	Bukkit.getServer().broadcastMessage("[Slack] " + name + ": " + msg);
+		    }
+		}else{
+			System.out.println("[SlackChat] Incoming message not verified");
+		}
+	}
+
+}
+class ThreadSocket extends Thread {
+    private Socket insocket;
+    ThreadSocket(Socket insocket) {
+        this.insocket = insocket;
+        this.start();
+    }
+    @Override
+    public void run() {
+        try {
+            InputStream is = insocket.getInputStream();
+            PrintWriter out = new PrintWriter(insocket.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(is));
+            String line;
+            line = in.readLine();
+            String request_method = line;
+            //System.out.println("HTTP-HEADER: " + line);
+            line = "";
+            // looks for post data
+            int postDataI = -1;
+            while ((line = in.readLine()) != null && (line.length() != 0)) {
+                //System.out.println("HTTP-HEADER: " + line);
+                if (line.indexOf("Content-Length:") > -1) {
+                    postDataI = new Integer(
+                            line.substring(
+                                    line.indexOf("Content-Length:") + 16,
+                                    line.length())).intValue();
+                }
+            }
+            String postData = "";
+            // read the post data
+            if (postDataI > 0) {
+                char[] charArray = new char[postDataI];
+                in.read(charArray, 0, postDataI);
+                postData = new String(charArray);
+            }
+            out.println("HTTP/1.0 200 OK");
+            out.println("Content-Type: text/html; charset=utf-8");
+            out.println("Server: MINISERVER");
+            // this blank line signals the end of the headers
+            out.println("");
+            // Send the HTML page
+            out.println("<h1>Shenanigans</h1>");
+            out.println("<h2>Request Method->" + request_method + "</h2>");
+            out.println("<h2>Post->" + postData + "</h2>");
+            out.close();
+            insocket.close();
+            
+            Slack.comm.chat(postData);
+        } catch (IOException e) {
+        	System.out.println("[SlackChat] " + e.getStackTrace());
+        }
+    }
+}
---
+++ com/norway240/slack/Slack.java
@@ -0,0 +1,116 @@
+package com.norway240.slack; 
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Slack extends JavaPlugin implements Listener {
+	
+	public static Comms comm;
+	File configFile;
+    FileConfiguration config;
+
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+		Plugin plugin = Bukkit.getPluginManager().getPlugin("SlackChat");
+		configFile = new File(plugin.getDataFolder(), "config.yml");
+		
+		try {
+	        firstRun();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    config = new YamlConfiguration();
+	    loadYamls();
+		
+	    comm.receive();
+		getLogger().info("SlackChat enabled! (a plugin by norway240)");
+	}
+
+	public void onDisable() {
+		comm.close();
+		getLogger().info("SlackChat disabled! (a plugin by norway240)");
+	}
+	
+	private void firstRun(){
+		if(!configFile.exists()){
+	        configFile.getParentFile().mkdirs();
+	        copy(getResource("config.yml"), configFile);
+	    }
+	}
+	
+	private void copy(InputStream in, File file) {
+	    try {
+	        OutputStream out = new FileOutputStream(file);
+	        byte[] buf = new byte[1024];
+	        int len;
+	        while((len=in.read(buf))>0){
+	            out.write(buf,0,len);
+	        }
+	        out.close();
+	        in.close();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	public void loadYamls() {
+	    try {
+	        config.load(configFile);
+		    
+		    String w = config.getString("webhook");
+		    if(w.contains("CONFIGURETHIS")){
+		    	getServer().broadcast("[SlackChat] SlackChat must be configured", Server.BROADCAST_CHANNEL_ADMINISTRATIVE);
+		    }else{
+			    String v = config.getString("verification");
+			    int p = config.getInt("port");
+			    
+				comm = new Comms(w, v, p);
+		    }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+
+    String msg = "", name = "";
+    
+	@EventHandler
+	public void playerChat(AsyncPlayerChatEvent  event){
+	    msg = event.getMessage();
+	    name = event.getPlayer().getName();
+	    new Thread(new Runnable() {
+	        public void run(){
+			    try{
+					comm.send(name, msg);
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+	        }
+	    }).start();
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("slackchat")){
+			loadYamls();
+			sender.sendMessage("[SlackChat] Configuration reloaded");
+			return true;
+		}
+		return false;
+	}
+	
+}
